f69365f2f11a7095d9df2d72443559a3ff385284
sepi0l <sepi0l@users.noreply.github.com>
2017-03-09 21:04:36 +0300

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <cstdlib>
-using namespase std;
 
 int main(int argc, char const *argv[]) {
-    cout << "Hello World!" << endl;
+    std::cout << "Hello World!" << endl;
     system("pause");
     return 0;
 }
