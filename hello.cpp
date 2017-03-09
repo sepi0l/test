a31d0eed25087fd2411282677ccf512f484f700f
dell <dell@localhost>
2017-03-09 18:46:14 +0300

    First commit

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cstdlib>
+using namespase std;
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello World!" << endl;
+    system("pause");
+    return 0;
+}
